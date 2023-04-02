9431c12d2eb938ebd93a39c2bd2c04a24ccabb46
TheRayanFikri <129669770+TheRayanFikri@users.noreply.github.com>
2023-04-02 15:36:53 +0200

    Update .lua

--- .lua
+++ .lua
@@ -1,41 +1,44 @@
-# Compiled Lua sources
-luac.out
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua%22))()
+local Window = Library.CreateLib("DrinkoHub", "Midnight")
 
-# luarocks build files
-*.src.rock
-*.zip
-*.tar.gz
+local Tab1 = Window:NewTab("Main")
+local Tab2 = Window:NewTab("Teleport")
+local Tab3 = Window:NewTab("Player")
 
-# Object files
-*.o
-*.os
-*.ko
-*.obj
-*.elf
+local Section1 = Tab1:NewSection("Fly")
+local Section2 = Tab1:NewSection("SuperHuman")
+local Section3 = Tab1:NewSection("InfiniteYield")
+local Section4 = Tab3:NewSection("Walkspeed And JumpPower")
 
-# Precompiled Headers
-*.gch
-*.pch
 
-# Libraries
-*.lib
-*.a
-*.la
-*.lo
-*.def
-*.exp
+Section1:NewButton("Fly", "Makes you flying", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Nicuse/RobloxScripts/main/BypassedFly.lua%22))()
 
-# Shared objects (inc. Windows DLLs)
-*.dll
-*.so
-*.so.*
-*.dylib
+    Fly(true)
+end)
 
-# Executables
-*.exe
-*.out
-*.app
-*.i*86
-*.x86_64
-*.hex
+Section2:NewToggle("Super Human", "Increase You're WalkSpeed and JumpPower", function(state)
+    if state then
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 120
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 120
+    else
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
 
+    end
+end)
+Section3:NewButton("InfiniteYield", "Gives you admin Power", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source%27),true))()
+end)
+
+Section4:NewSlider("WalkSpeed", "Customize you're WalkSpeed", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section4:NewSlider("JumapPower", "Customize you're JumpPower", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+Section4:NewButton("Reset", "Reset you're walkspeed and JumpPower to normal", function()
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = 16
+end)
